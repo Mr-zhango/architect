1dee7938fc19091cb7342d609c6944b2bbdcef66
zhangyang <zhangyang@ec.e-chemchina.com>
2020-04-07 16:09:12 +0800

    Volatile  原子性问题解决

--- 03_multi_thread_lock/src/main/java/cn/myfreecloud/volatiledemo/VolatileNotAtomicSolution.java
+++ 03_multi_thread_lock/src/main/java/cn/myfreecloud/volatiledemo/VolatileNotAtomicSolution.java
@@ -1,5 +1,7 @@
 package cn.myfreecloud.volatiledemo;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author: zhangyang
  * @date: 2020/4/7 16:05
@@ -7,6 +9,34 @@ package cn.myfreecloud.volatiledemo;
  */
 public class VolatileNotAtomicSolution {
     public static void main(String[] args) {
+        // 初始化能够装10个线程的容器--数组
+        VolatileNotAtomicSolutionThread[] volatileNotAtomicArray = new VolatileNotAtomicSolutionThread[10];
+        for (int i = 0; i < volatileNotAtomicArray.length; i++) {
 
+            // 创建线程
+            volatileNotAtomicArray[i] = new VolatileNotAtomicSolutionThread();
+        }
+
+        for (int i = 0; i < volatileNotAtomicArray.length; i++) {
+
+            // 启动线程
+            volatileNotAtomicArray[i].start();
+        }
+
+
+    }
+}
+
+class VolatileNotAtomicSolutionThread extends Thread {
+
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            // 相当于 atomicInteger ++
+            atomicInteger.incrementAndGet();
+        }
+        System.out.println(getName() + "-------" + atomicInteger.get());
     }
 }
