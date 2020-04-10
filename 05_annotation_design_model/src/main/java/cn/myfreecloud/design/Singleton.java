2310a84f3f1ca3b0bfc452b8ab5eff73b865b12c
zhangyang <zhangyang@ec.e-chemchina.com>
2020-04-11 00:00:09 +0800

    两种常见单例模式的写法 懒汉式,线程不安全 饿汉式,线程安全

---
+++ 05_annotation_design_model/src/main/java/cn/myfreecloud/design/Singleton.java
@@ -0,0 +1,43 @@
+package cn.myfreecloud.design;
+
+/**
+ * 懒汉式代码
+ * 线程不安全,需要加上同步锁 但是效率高
+ */
+class SingletonTest {
+
+    /**
+     * 单例:保证这个类在jvm中只有一个
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Singleton sl1 = Singleton.getSingleton();
+        Singleton sl2 = Singleton.getSingleton();
+        System.out.println(sl1 == sl2);
+    }
+}
+
+public class Singleton {
+
+    // 当需要的才会被实例化
+    private static Singleton singleton;
+
+    /**
+     * 反射也不能初始化了
+     */
+    private Singleton() {
+
+    }
+
+    public static Singleton getSingleton() {
+
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                singleton = new Singleton();
+            }
+        }
+        return singleton;
+    }
+
+}
---
+++ 05_annotation_design_model/src/main/java/cn/myfreecloud/design/Singleton1.java
@@ -0,0 +1,28 @@
+package cn.myfreecloud.design;
+
+
+/**
+ * 饿汉式,天生线程安全
+ * 因为当class文件被加载的时候,被初始化了
+ */
+class SingletonTest1 {
+	public static void main(String[] args) {
+		Singleton1 sl1 = Singleton1.getSingleton();
+		Singleton1 sl2 = Singleton1.getSingleton();
+		System.out.println((sl1 == sl2)+"-");
+	}
+}
+
+public class Singleton1 {
+	//当class 文件被加载初始化
+	private static Singleton1 singleton = new Singleton1();
+
+	private Singleton1() {
+
+	}
+
+	public static Singleton1 getSingleton() {
+		return singleton;
+	}
+
+}
