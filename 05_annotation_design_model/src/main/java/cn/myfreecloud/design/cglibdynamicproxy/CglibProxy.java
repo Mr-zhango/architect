59e09e5a4ee733d3afccf7815f4f1e08e29755f7
zhangyang <zhangyang@ec.e-chemchina.com>
2020-04-11 17:59:31 +0800

    代理模式:cglib动态代理

---
+++ 05_annotation_design_model/src/main/java/cn/myfreecloud/design/cglibdynamicproxy/CglibProxy.java
@@ -0,0 +1,42 @@
+package cn.myfreecloud.design.cglibdynamicproxy;
+
+import cn.myfreecloud.design.proxy.House;
+import cn.myfreecloud.design.proxy.HouseBuyer;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author: zhangyang
+ * @date: 2020/4/11 17:20
+ * @description:
+ */
+public class CglibProxy implements MethodInterceptor {
+
+    @Override
+    public Object intercept(Object object, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+
+        System.out.println("我是jdk动态代理,开始监听了------");
+        Object invoke = methodProxy.invokeSuper(object, objects);
+        System.out.println("我是jdk动态代理,已经结束监听了------");
+
+        return invoke;
+    }
+
+    public static void main(String[] args) {
+        CglibProxy cglibProxy = new CglibProxy();
+
+        // 使用asm框架生成代理类
+        Enhancer enhancer = new Enhancer();
+
+        enhancer.setSuperclass(HouseBuyer.class);
+
+        enhancer.setCallback(cglibProxy);
+
+        House house = (House) enhancer.create();
+
+        house.mai();
+    }
+}
